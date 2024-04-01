9afecaa23af32b7f990875936d97fcfd7a668b9b
UttkarshSharma23 <uttkarsh.sharma2019@gmail.com>
2024-04-01 10:51:42 +0530

    problem 1 added

---
+++ day1.js
@@ -0,0 +1,16 @@
+/* DAY 1
+Question : Longest word in a String
+
+Q: write a function that takes a string as input and returns the longest word as the string. If there are multiple longest WebTransportDatagramDuplexStream, return the first encountered.
+
+//* Constraints
+>>The input string may contain alphabetic characters, digits,spaces and punctuations.
+>>The input string is non-empty
+>>The input string may contain multiple words seperated by spaces
+
+//* Note:
+>>If the input string is empty or contains only whitespace,the function should return as false.
+>>The function should ignore leading the trailing whitespace when determining the longest word.
+*/
+
+console.log(findLongestWord("Watch Uttkarsh Sharma javascript 100 days practice on Github"))
